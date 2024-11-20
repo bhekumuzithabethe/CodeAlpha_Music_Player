b7315afdf43fee608a6e0c459b4812ae7453a0d8
Bhekumuzi Thabethe <4123449@myuwc.ac.za>
2024-11-20 16:57:45 +0200

    add music folder, prev and next button funtionality

--- scripts.js
+++ scripts.js
@@ -4,6 +4,43 @@ let play = document.getElementById('play-icon');
 let next = document.getElementById('next');
 let back = document.getElementById('back');
 let title = document.getElementById('song-title');
+let song_cover = document.getElementById('audio-cover');
+
+//song titles 
+const songs = ['gucci','emabhukwini','emhlangeni'];
+
+let songIndex = 0
+
+loadSong(songs[songIndex]);
+
+function loadSong(song){
+    title.innerText = song;
+    song.src = `music/${song}.mp3`;
+    song_cover.src = `images/${song}.jpeg`;
+
+}
+function prevSong() {
+    songIndex--;
+
+    if(songIndex<0){
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+    playPause();
+}
+
+
+function nextSong() {
+    songIndex++;
+
+    if(songIndex>songs.length -1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playPause();
+}
+
+
 
 progress.onchange = function(){
     song.play;
@@ -17,65 +54,23 @@ song.onloadedmetadata = function(){
     progress.value = song.currentTime;
 }
 
+//Seeting the progress bar to the current minutes and seconds of the song
 if(song.play()){
     setInterval(()=>{
         progress.value = song.currentTime;
     },500);
 }
-
+//Playing and pausing the song
 function playPause() {
     if(play.classList.contains('fa-pause')){
         song.pause();
         play.classList.remove('fa-pause');
-        play.classList.add('fa-play')
+        play.classList.add('fa-play');
     }
     else{
         song.play();
         play.classList.remove('fa-play');
-        play.classList.add('fa-pause')
+        play.classList.add('fa-pause');
     }
 }
 
-function prevSong(params) {
-    
-}
-
-
-
-//song titles 
-const songs = ['gucci','umfana wezinkomo'];
-
-let songIndex = 0
-
-loadSong(songs[sonIndex]);
-
-function loadSong(song){
-    title.innerText = song;
-    audio = `media/${song}.mp3`
-    audio = `media/${song}.jpeg`
-
-}
-function prevSong() {
-    songIndex++;
-
-    if(songIndex>songs.length -1){
-        songIndex = 0;
-    }
-    loadSong(songs[songIndex]);
-    playPause();
-}
-
-
-function nextSong() {
-    songIndex--;
-
-    if(songIndex<0){
-        songIndex = songs.length -1;
-    }
-    loadSong(songs[songIndex]);
-    playPause();
-}
-
-
-next.addEventListener('click', nextSong);
-back.addEventListener('click', prevSong);
