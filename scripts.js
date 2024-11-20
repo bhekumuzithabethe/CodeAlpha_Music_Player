c7066554228abcb9541d41da2490fb790ecb43a1
Bhekumuzi Thabethe <4123449@myuwc.ac.za>
2024-11-20 18:03:19 +0200

    add next,prev buttons and images

--- scripts.js
+++ scripts.js
@@ -6,71 +6,68 @@ let back = document.getElementById('back');
 let title = document.getElementById('song-title');
 let song_cover = document.getElementById('audio-cover');
 
-//song titles 
-const songs = ['gucci','emabhukwini','emhlangeni'];
-
-let songIndex = 0
+// Song titles
+const songs = ["Gucci (feat MaWhoo)", "Emabhukwini Amafa", "ZING'SHIYIL' EZIY' EMHLANGENI"];
+let songIndex = 0;
 
 loadSong(songs[songIndex]);
 
-function loadSong(song){
-    title.innerText = song;
-    song.src = `music/${song}.mp3`;
-    song_cover.src = `images/${song}.jpeg`;
-
+function loadSong(songName) {
+    title.innerText = songName;
+    song.src = `music/${songName}.mp3`;
+    song_cover.src = `images/${songName}.jpeg`;
 }
+
 function prevSong() {
     songIndex--;
-
-    if(songIndex<0){
-        songIndex = songs.length -1;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
     }
     loadSong(songs[songIndex]);
     playPause();
 }
 
-
 function nextSong() {
     songIndex++;
-
-    if(songIndex>songs.length -1){
+    if (songIndex > songs.length - 1) {
         songIndex = 0;
     }
     loadSong(songs[songIndex]);
     playPause();
+
 }
 
-
-
-progress.onchange = function(){
-    song.play;
+progress.onchange = function () {
     song.currentTime = progress.value;
+    song.play();
     play.classList.remove('fa-play');
-        play.classList.add('fa-pause')
+    play.classList.add('fa-pause');
 }
 
-song.onloadedmetadata = function(){
+song.onloadedmetadata = function () {
     progress.max = song.duration;
     progress.value = song.currentTime;
 }
 
-//Seeting the progress bar to the current minutes and seconds of the song
-if(song.play()){
-    setInterval(()=>{
-        progress.value = song.currentTime;
-    },500);
+// Updating progress bar every 500ms
+song.ontimeupdate = function () {
+    progress.value = song.currentTime;
 }
-//Playing and pausing the song
+
+// Playing and pausing the song
 function playPause() {
-    if(play.classList.contains('fa-pause')){
+    if (play.classList.contains('fa-pause')) {
         song.pause();
         play.classList.remove('fa-pause');
         play.classList.add('fa-play');
-    }
-    else{
+    } else {
         song.play();
         play.classList.remove('fa-play');
         play.classList.add('fa-pause');
     }
 }
 
+// Event listeners for next and previous buttons
+next.addEventListener('click', nextSong);
+back.addEventListener('click', prevSong);
+play.addEventListener('click', playPause);
